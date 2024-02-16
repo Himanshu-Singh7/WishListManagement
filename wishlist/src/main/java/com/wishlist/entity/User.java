46edb7b6772618103ac8968900fb338771a644b7
Himanshu <himanshusingh0608@gmail.com>
2024-02-16 18:39:12 +0530

    JWT spring security

--- wishlist/src/main/java/com/wishlist/entity/User.java
+++ wishlist/src/main/java/com/wishlist/entity/User.java
@@ -2,6 +2,7 @@ package com.wishlist.entity;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Set;
 import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
@@ -9,6 +10,9 @@ import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
 import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import lombok.Getter;
@@ -26,7 +30,7 @@ public class User {
 	@Id
 	@GeneratedValue(strategy = GenerationType.AUTO)
 	private Integer userId;
-	@Column(name = "user_name")
+	@Column(name = "user_name",unique = true)
 	private String name;
 	@Column(name= "email")
 	private String email;
@@ -36,4 +40,12 @@ public class User {
 	@OneToMany(mappedBy = "user" ,cascade = CascadeType.ALL , fetch = FetchType.LAZY)
 	private List<Wishlist> wishlist = new ArrayList<Wishlist>();
 
+	
+	@ManyToMany(fetch = FetchType.EAGER,cascade = CascadeType.ALL)
+    @JoinTable(
+            name = "user_roles",
+            joinColumns = @JoinColumn(name = "user_id" , referencedColumnName = "userId"),
+            inverseJoinColumns = @JoinColumn(name = "role_id", referencedColumnName = "id")
+    )
+    private Set<Role> roles;
 }
--- wishlist/src/main/java/com/wishlist/entity/Wishlist.java
+++ wishlist/src/main/java/com/wishlist/entity/Wishlist.java
@@ -32,7 +32,7 @@ public class Wishlist {
 	private Date wishlistDate;
 	
 	@ManyToOne
-    @JoinColumn(name ="user_id")
+    @JoinColumn(name ="user_id" , nullable = false)
 	private User user;
 	
 	
